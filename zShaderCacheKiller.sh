d1e519ce108daff5295f89a6c42b8f63e8503de4
introkun <introkun@gmail.com>
2024-07-04 12:30:57 -0300

    refactoring of zShaderCacheKiller.sh - added stdout logging for debug purposes - added warning in case of multi-line symlink and ignored this case - fixed warnings about stray \ before " - do not call du multiple times (script speed up and consistency)

--- zShaderCacheKiller.sh
+++ zShaderCacheKiller.sh
@@ -11,48 +11,92 @@ fi
 
 #live=0 #uncomment for debugging/testing
 
+function log_debug () {
+  if [ $live = 0 ]; then
+    echo "$1"
+  fi
+}
+
+function info () {
+  zenity --info --width=400 --text="$1"
+}
+
+function warning () {
+  zenity --warning --width=400 --text="$1"
+  log_debug "Warning: $1"
+}
+
+function info_with_timeout () {
+  zenity --info --width=400 --text="$1" --timeout=1
+}
+
+function error () {
+  zenity --error --width=400 --text="$1"
+  log_debug "Error: $1"
+}
+
+log_debug "Dry-Run: $live"
+
 #script_dir="$(dirname $(realpath "$0"))"
 #conf_dir="$(dirname $(realpath "$0"))/config"
 conf_dir="/tmp/scawp.SDCacheKiller"
 tmp_dir="/tmp/scawp.SDCacheKiller"
 steamapps_dir="/home/deck/.local/share/Steam/steamapps"
 
-#create tempory directory
+log_debug "Creating temporary directories..."
 mkdir -p "$tmp_dir"
 mkdir -p "$conf_dir"
+log_debug "Temporary directory created: $tmp_dir and $conf_dir"
 
-#download list of all steam ids if we havn't already
+log_debug "Downloading list of all Steam IDs if we haven't already"
+#TODO: Download new list every week?
 if [ ! -f "$conf_dir/fulllist.json" ] || [ ! -s "$conf_dir/fulllist.json" ];then 
   #TODO: Test when offline or errorcode
   curl "https://api.steampowered.com/ISteamApps/GetAppList/v2/" > "$conf_dir/fulllist.json"
+  log_debug "Downloaded list of all Steam IDs!"
 fi
 
-#check we can find the steamapps directory
+log_debug "Checking for Steam Library locations..."
 if [ ! -d "$steamapps_dir" ]; then
-  zenity --error --width=400 \
-  --text="Cannot find $steamapps_dir, Quitting!"
+  error "Cannot find $steamapps_dir, Quitting!"
   exit 1;
 fi
 
+log_debug "Found Steam Library locations!"
 #find all of the steam library locations
-steamapp_dir=( $(grep -ho '\"path\"\s*\".*\"' "$steamapps_dir/libraryfolders.vdf" | sed -e 's/^\"path\"\s*\"//' -e 's/\"$/\/steamapps/') )
+steamapp_dir=( $(grep -ho '"path"\s*".*"' "$steamapps_dir/libraryfolders.vdf" | sed -e 's/^"path"\s*"//' -e 's/"$/\/steamapps/') )
+log_debug "Steam Library locations: ${steamapp_dir[@]}"
 
 function get_list () {
+  log_debug "Getting list of $1..."
   true > "$tmp_dir/tmp_list.txt"
+  disk_usage=$(du -m --max-depth 0  "$steamapps_dir/$1"/* | sort -nr)
   du -m --max-depth 0  "$steamapps_dir/$1"/* | sort -nr > "$tmp_dir/tmp_list2.txt"
-  
-  #Check for symlinked folders
+
+  log_debug "Check for symlinked folders"
   while read -r path; do
-    if [ -h $path ] && [ ! -e $path ]; then
-      echo -e "?\t$(readlink $path)" >> "$tmp_dir/tmp_list.txt"
+    if [ -h $path ]; then
+      readlink_output=$(readlink "$path")
+      if [ ! -e $path ]; then
+        log_debug "Broken symlink $path pointing to $readlink_output"
+      else
+        log_debug "Symlink $path pointing to $readlink_output"
+        line_count=$(echo "$readlink_output" | wc -l)
+        if [ ! "$line_count" -eq 1 ]; then
+          warning "Symlink $path points to multiple locations: $readlink_output. Ignoring."
+        else
+          echo -e "?\t$readlink_output" >> "$tmp_dir/tmp_list.txt"
+        fi
+      fi
     else
+      log_debug "Realpath $path is $(realpath $path)"
       du -m --max-depth 0  "$(realpath $path)" >> "$tmp_dir/tmp_list.txt"
     fi
   done <<< "$(awk '{ print $2 }' "$tmp_dir/tmp_list2.txt")"
 
-  du -m --max-depth 0  "$steamapps_dir/$1"/* | sort -nr | sed 's/^.*\///' > "$tmp_dir/tmp_ids.txt"
+  echo "$disk_usage" | sed 's/^.*\///' > "$tmp_dir/tmp_ids.txt"
 
-  du -m --max-depth 0  "$steamapps_dir/$1"/* | sort -nr | sed -e 's/^.*\///' -e 's/^/appmanifest_/' -e 's/$/\.acf/' > "$tmp_dir/tmp_col_manifest.txt"
+  echo "$disk_usage" | sed -e 's/^.*\///' -e 's/^/appmanifest_/' -e 's/$/\.acf/' > "$tmp_dir/tmp_col_manifest.txt"
 
   true > "$tmp_dir/tmp_names.txt"
 
@@ -62,7 +106,7 @@ function get_list () {
 
     for dir in "${steamapp_dir[@]}"; do 
       if [ -s  "$dir/$manifest" ]; then
-        install_dir="$(grep -ho '\"installdir\"\s*\".*\"' "$dir/$manifest" | sed -e 's/^\"installdir\"\s*\"//' -e 's/\"$//')"
+        install_dir="$(grep -ho '"installdir"\s*".*"' "$dir/$manifest" | sed -e 's/^"installdir"\s*"//' -e 's/"$//')"
         echo -e "$install_dir\t " >> "$tmp_dir/tmp_names.txt"
         found=1
         break;
@@ -88,7 +132,7 @@ function get_list () {
         #try in content_log.txt also
         find_in_log="$(grep -ri "SteamLaunch\sAppId=$app_id" ~/.local/share/Steam/logs/content_log.txt | tail -n 1)"
         if [ ! -z "$find_in_log" ];then
-          echo -e "$find_in_log\tNon-Steam" | sed -e "s/.*[=|\/]//g" -e "s/\"//g" >> "$tmp_dir/tmp_names.txt"
+          echo -e "$find_in_log\tNon-Steam" | sed -e "s/.*[=|\/]//g" -e 's/"//g' >> "$tmp_dir/tmp_names.txt"
         else
           #we don't know
           echo -e "Unknown\tNon-Steam" >> "$tmp_dir/tmp_names.txt"
