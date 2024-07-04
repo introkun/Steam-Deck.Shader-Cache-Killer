363d67f2596222321bf4d73c9eab5eb871b139a9
introkun <introkun@gmail.com>
2024-07-04 18:28:54 -0300

    trimmed non-printable characters - returned old behavior for symlinks

--- zShaderCacheKiller.sh
+++ zShaderCacheKiller.sh
@@ -71,7 +71,7 @@ function get_list () {
   log_debug "Getting list of $1..."
   true > "$tmp_dir/tmp_list.txt"
   disk_usage=$(du -m --max-depth 0  "$steamapps_dir/$1"/* | sort -nr)
-  du -m --max-depth 0  "$steamapps_dir/$1"/* | sort -nr > "$tmp_dir/tmp_list2.txt"
+  echo "$disk_usage" > "$tmp_dir/tmp_list2.txt"
 
   log_debug "Check for symlinked folders"
   while read -r path; do
@@ -85,7 +85,9 @@ function get_list () {
         if [ ! "$line_count" -eq 1 ]; then
           warning "Symlink $path points to multiple locations: $readlink_output. Ignoring."
         else
-          echo -e "?\t$readlink_output" >> "$tmp_dir/tmp_list.txt"
+          # TODO: New non-tested behavior
+          #echo -e "?\t$readlink_output" >> "$tmp_dir/tmp_list.txt"
+          du -m --max-depth 0  "$(realpath $path)" >> "$tmp_dir/tmp_list.txt"
         fi
       fi
     else
@@ -102,9 +104,10 @@ function get_list () {
 
   while read -r manifest; do
     found=0
+    log_debug "Checking $manifest"
     app_id="$(echo "$manifest" | sed -e "s/appmanifest_//" -e "s/\.acf//")"
 
-    for dir in "${steamapp_dir[@]}"; do 
+    for dir in "${steamapp_dir[@]}"; do
       if [ -s  "$dir/$manifest" ]; then
         install_dir="$(grep -ho '"installdir"\s*".*"' "$dir/$manifest" | sed -e 's/^"installdir"\s*"//' -e 's/"$//')"
         echo -e "$install_dir\t " >> "$tmp_dir/tmp_names.txt"
@@ -115,7 +118,8 @@ function get_list () {
     #TODO: This is slow, cache the results?
     if [ $found = 0 ] && [ -f "$conf_dir/fulllist.json" ] && [ -s "$conf_dir/fulllist.json" ] && [ "$app_id" -lt 100000000 ];then
       app_name="$(cat "$conf_dir/fulllist.json" | jq -r ".applist.apps[] | select(.appid == $app_id) | .name"  | head -n1)"
-      if [ ! -z "$app_name" ];then 
+      if [ ! -z "$app_name" ]; then
+        app_name=$(echo "$app_name" | tr -cd '[:print:]')
         echo -e "$app_name\tUninstalled?" >> "$tmp_dir/tmp_names.txt"
         found=1
       fi
@@ -124,15 +128,17 @@ function get_list () {
     if [ $found = 0 ]; then
       #Non-steam games might be found by checking the controller_ui.txt logs
       app_id="$(echo "$manifest" | sed -e "s/appmanifest_//" -e "s/\.acf//")"
-      find_in_log="$(grep -ri "AppID\s$app_id," ~/.local/share/Steam/logs/controller_ui.txt | tail -n 1)"
+      find_in_log="$(grep -ri "AppID\s$app_id," ~/.local/share/Steam/logs/controller_ui.txt | tail -n 1 | tr -cd '[:print:]')"
       
-      if [ ! -z "$find_in_log" ];then
-       echo -e "$find_in_log\tNon-Steam" | sed -e "s/.*,\s//" >> "$tmp_dir/tmp_names.txt"
+      if [ ! -z "$find_in_log" ]; then
+        echo -e "$find_in_log\tNon-Steam" | sed -e "s/.*,\s//" >> "$tmp_dir/tmp_names.txt"
       else
         #try in content_log.txt also
         find_in_log="$(grep -ri "SteamLaunch\sAppId=$app_id" ~/.local/share/Steam/logs/content_log.txt | tail -n 1)"
-        if [ ! -z "$find_in_log" ];then
-          echo -e "$find_in_log\tNon-Steam" | sed -e "s/.*[=|\/]//g" -e 's/"//g' >> "$tmp_dir/tmp_names.txt"
+        if [ ! -z "$find_in_log" ]; then
+          app_name=$(echo "$find_in_log" | sed -e "s/.*[=|\/]//g" -e 's/"//g' -e 's/^ //g' | tr -cd '[:print:]' | tr -d '[]')
+          log_debug "Found $app_name application"
+          echo -e "$app_name\tNon-Steam" >> "$tmp_dir/tmp_names.txt"
         else
           #we don't know
           echo -e "Unknown\tNon-Steam" >> "$tmp_dir/tmp_names.txt"
